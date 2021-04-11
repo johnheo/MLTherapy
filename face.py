9d0753246234f4376a0b5646aa70759480bab586
Jung Hwan Heo <johnheo1128112@gmail.com>
2021-04-10 23:26:42 -0700

    modifications

--- face.py
+++ face.py
@@ -14,6 +14,7 @@ params = {
 
 #getting the emotions from api
 def get_emotions(img_path): 
+
     image_data = open(img_path, "rb")
     response = requests.post(face_api_url, params=params, headers=headers, data=image_data)
     response.raise_for_status()
@@ -22,10 +23,20 @@ def get_emotions(img_path):
     try: 
         emotions = data[0]["faceAttributes"]["emotion"]
         #print(json.dumps(emotions, sort_keys=True, indent=2))
-        return emotions
+        neutral = emotions["neutral"]
+        contempt = emotions["contempt"]
+        sadness = emotions["sadness"]
+        happiness = emotions["happiness"]
+        mood = ['negative', 'positive', 'neutral']
+        if (contempt > .4 or sadness > .4 or contempt+sadness >.4):
+            return mood[0]
+        elif (happiness >.5):
+            return mood[1]
+        else:
+            return mood[2]
     except:
         if not data:
-            print("no face detected ")
+            print("no face... ")
         else:
             print(json.dumps(emotions, sort_keys=True, indent=2))
         return 0
